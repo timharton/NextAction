37146551f9001aa24e7b8a0610e37aea1ae50667
Tim Harton <tim@harton.co>
2016-03-08 14:10:20 +1300

    I have refactored the code into a preliminary classes which appea

--- nextaction.py
+++ nextaction.py
@@ -10,29 +10,71 @@ import time
 import sys
 from datetime import datetime
 
-
-def get_subitems(items, parent_item=None):
-    """Search a flat item list for child items"""
-    result_items = []
-    found = False
-    if parent_item:
-        required_indent = parent_item['indent'] + 1
-    else:
-        required_indent = 1
-    for item in items:
+class TodoistConnection(object):
+    """docstring for TodoistConnection"""
+    def __init__(self, args, api, logging):
+        super(TodoistConnection, self).__init__()
+        self.args = args
+        self.api = api
+        self.logging = logging
+        self.label = None
+        
+    def get_subitems(self, items, parent_item=None):
+        """Search a flat item list for child items"""
+        result_items = []
+        found = False
         if parent_item:
-            if not found and item['id'] != parent_item['id']:
-                continue
-            else:
-                found = True
-            if item['indent'] == parent_item['indent'] and item['id'] != parent_item['id']:
-                return result_items
-            elif item['indent'] == required_indent and found:
-                result_items.append(item)
-        elif item['indent'] == required_indent:
-            result_items.append(item)
-    return result_items
+            required_indent = parent_item['indent'] + 1
+        else:
+            required_indent = 1
 
+        for item in items:
+            if parent_item:
+                if not found and item['id'] != parent_item['id']:
+                    continue
+                else:
+                    found = True
+                if item['indent'] == parent_item['indent'] and item['id'] != parent_item['id']:
+                    return result_items
+                elif item['indent'] == required_indent and found:
+                    result_items.append(item)
+            elif item['indent'] == required_indent:
+                result_items.append(item)
+        return result_items
+
+    def get_project_type(self, project_object):
+        """Identifies how a project should be handled"""
+        name = project_object['name'].strip()
+        if name == 'Inbox':
+            return self.args.inbox
+        elif name[-1] == self.args.parallel_suffix:
+            return 'parallel'
+        elif name[-1] == self.args.serial_suffix:
+            return 'serial'
+
+    def get_item_type(self, item):
+        """Identifies how a item with sub items should be handled"""
+        name = item['content'].strip()
+        if name[-1] == self.args.parallel_suffix:
+            return 'parallel'
+        elif name[-1] == self.args.serial_suffix:
+            return 'serial'
+
+    def add_label(self, item):
+        if self.label not in item['labels']:
+            labels = item['labels']
+            self.logging.debug('Updating %s with label', item['content'])
+            labels.append(self.label)
+            self.api.items.update(item['id'], labels=labels)
+
+    def remove_label(self, item):
+        if self.label in item['labels']:
+            labels = item['labels']
+            self.logging.debug('Updating %s without label', item['content'])
+            labels.remove(self.label)
+            self.api.items.update(item['id'], labels=labels)
+
+    # Main loop
 
 def main():
 
@@ -65,114 +107,88 @@ def main():
     # Run the initial sync
     logging.debug('Connecting to the Todoist API')
     api = TodoistAPI(token=args.api_key)
-    logging.debug('Syncing the current state from the API')
-    api.sync(resource_types=['projects', 'labels', 'items'])
+    conn = TodoistConnection(args, api, logging)
+
+    conn.logging.debug('Syncing the current state from the API')
+    conn.api.sync(resource_types=['projects', 'labels', 'items'])
 
     # Check the next action label exists
-    labels = api.labels.all(lambda x: x['name'] == args.label)
+    labels = conn.api.labels.all(lambda x: x['name'] == args.label)
     if len(labels) > 0:
         label_id = labels[0]['id']
-        logging.debug('Label %s found as label id %d', args.label, label_id)
+        conn.logging.debug('Label %s found as label id %d', args.label, label_id)
     else:
-        logging.error("Label %s doesn't exist, please create it or change TODOIST_NEXT_ACTION_LABEL.", args.label)
+        conn.logging.error("Label %s doesn't exist, please create it or change TODOIST_NEXT_ACTION_LABEL.", args.label)
         sys.exit(1)
 
-    def get_project_type(project_object):
-        """Identifies how a project should be handled"""
-        name = project_object['name'].strip()
-        if project['name'] == 'Inbox':
-            return args.inbox
-        elif name[-1] == args.parallel_suffix:
-            return 'parallel'
-        elif name[-1] == args.serial_suffix:
-            return 'serial'
+    conn.label = label_id
 
-    def get_item_type(item):
-        """Identifies how a item with sub items should be handled"""
-        name = item['content'].strip()
-        if name[-1] == args.parallel_suffix:
-            return 'parallel'
-        elif name[-1] == args.serial_suffix:
-            return 'serial'
-
-    def add_label(item, label):
-        if label not in item['labels']:
-            labels = item['labels']
-            logging.debug('Updating %s with label', item['content'])
-            labels.append(label)
-            api.items.update(item['id'], labels=labels)
-
-    def remove_label(item, label):
-        if label in item['labels']:
-            labels = item['labels']
-            logging.debug('Updating %s without label', item['content'])
-            labels.remove(label)
-            api.items.update(item['id'], labels=labels)
-
-    # Main loop
     while True:
         try:
-            api.sync(resource_types=['projects', 'labels', 'items'])
+            conn.api.sync(resource_types=['projects', 'labels', 'items'])
         except Exception as e:
-            logging.exception('Error trying to sync with Todoist API: %s' % str(e))
+            conn.logging.exception('Error trying to sync with Todoist API: %s' % str(e))
         else:
-            for project in api.projects.all():
-                project_type = get_project_type(project)
+            for project in conn.api.projects.all():
+                project_type = conn.get_project_type(project)
                 if project_type:
-                    logging.debug('Project %s being processed as %s', project['name'], project_type)
+                    conn.logging.debug('Project %s being processed as %s', project['name'], project_type)
 
-                    items = sorted(api.items.all(lambda x: x['project_id'] == project['id']), key=lambda x: x['item_order'])
+                    items = sorted(conn.api.items.all(lambda x: x['project_id'] == project['id']), key=lambda x: x['item_order'])
 
                     for item in items:
 
                         # If its too far in the future, remove the next_action tag and skip
-                        if args.hide_future > 0 and 'due_date_utc' in item.data and item['due_date_utc'] is not None:
+                        if conn.args.hide_future > 0 and 'due_date_utc' in item.data and item['due_date_utc'] is not None:
                             due_date = datetime.strptime(item['due_date_utc'], '%a %d %b %Y %H:%M:%S +0000')
                             future_diff = (due_date - datetime.utcnow()).total_seconds()
-                            if future_diff >= (args.hide_future * 86400):
-                                remove_label(item, label_id)
+                            if future_diff >= (conn.args.hide_future * 86400):
+                                conn.remove_label(item)
                                 continue
 
-                        item_type = get_item_type(item)
-                        child_items = get_subitems(items, item)
+                        item_type = conn.get_item_type(item)
+                        print 'item_type: ', item_type
+                        child_items = conn.get_subitems(items, item)
+                        print 'child items: ',child_items
                         if item_type:
-                            logging.debug('Identified %s as %s type', item['content'], item_type)
+                            conn.logging.debug('Identified %s as %s type', item['content'], item_type)
 
                         if item_type or len(child_items) > 0:
                             # Process serial tagged items
                             if item_type == 'serial':
                                 for idx, child_item in enumerate(child_items):
+                                    # print idx, child_item
                                     if idx == 0:
-                                        add_label(child_item, label_id)
+                                        conn.add_label(child_item)
                                     else:
-                                        remove_label(child_item, label_id)
+                                        conn.remove_label(child_item)
                             # Process parallel tagged items or untagged parents
                             else:
                                 for child_item in child_items:
-                                    add_label(child_item, label_id)
+                                    conn.add_label(child_item)
 
                             # Remove the label from the parent
-                            remove_label(item, label_id)
+                            conn.remove_label(item)
 
                         # Process items as per project type on indent 1 if untagged
                         else:
                             if item['indent'] == 1:
                                 if project_type == 'serial':
                                     if item['item_order'] == 1:
-                                        add_label(item, label_id)
+                                        conn.add_label(item)
                                     else:
-                                        remove_label(item, label_id)
+                                        conn.remove_label(item)
                                 elif project_type == 'parallel':
-                                    add_label(item, label_id)
+                                    conn.add_label(item)
 
-            logging.debug('%d changes queued for sync... commiting if needed', len(api.queue))
-            if len(api.queue):
-                api.commit()
+            conn.logging.debug('%d changes queued for sync... commiting if needed', len(conn.api.queue))
+            if len(conn.api.queue):
+                conn.api.commit()
 
-        if args.onetime:
+        if conn.args.onetime:
             break
-        logging.debug('Sleeping for %d seconds', args.delay)
-        time.sleep(args.delay)
+        conn.logging.debug('Sleeping for %d seconds', conn.args.delay)
+        time.sleep(conn.args.delay)
 
 
 if __name__ == '__main__':
